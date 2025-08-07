e104834456069e8d92074aad13e8e4581a772a88
Usman <ui.sabuwala@gmail.com>
2025-08-07 10:49:45 +0530

    added migration script

---
+++ migrate.php
@@ -0,0 +1,23 @@
+<?php
+$config = include __DIR__ . '/./config/config.php';
+
+$pdo = new PDO(
+  "mysql:host=" . $config['DB_HOST'] . ";dbname=" . $config['DB_NAME'],
+  $config['DB_USER'],
+  $config['DB_PASS']
+);
+
+// Simple way to run a SQL file
+function runMigration($file, $pdo) {
+    $sql = file_get_contents($file);
+    $pdo->exec($sql);
+    echo "Ran: $file\n";
+}
+
+$migrations = [
+    "migrations/001_create_test_table.sql"
+];
+
+foreach ($migrations as $migration) {
+    runMigration($migration, $pdo);
+}
