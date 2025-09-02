328b96a72e9da8d92df429857ab25a2ed2b07fcc
Victus44 <rennup4288@gmail.com>
2025-09-02 13:38:56 +0530

    Added dynmac changing html

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit;
-}
+$is_logged_in = isset($_SESSION['user_id']);
 ?>
-<h1>Welcome <?= $_SESSION['username'] ?>!</h1>
+
+<h1>Welcome!</h1>
+
+
+<ul>
+    <?php if ($is_logged_in): ?>
+        <li>Profile</li>
+        <li>Settings</li>
+        <li>Logout</li>
+    <?php else: ?>
+        <li>Login</li>
+        <li>Register</li>
+    <?php endif; ?>
+</ul>
