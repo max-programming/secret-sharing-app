2d4a2047ba3dc79cd61eda6a09ac7d24a2fea54a
Victus44 <rennup4288@gmail.com>
2025-09-03 13:53:49 +0530

    Modified index, about; added ui of profile

--- index.php
+++ index.php
@@ -3,16 +3,26 @@ session_start();
 $is_logged_in = isset($_SESSION['user_id']);
 ?>
 
-<h1>Welcome!</h1>
-
-
-<ul>
-    <?php if ($is_logged_in): ?>
-        <li>Profile</li>
-        <li>Settings</li>
-        <li>Logout</li>
-    <?php else: ?>
-        <li>Login</li>
+<?php if ($is_logged_in): ?>
+    <a href="#"><i class="fa-solid fa-user"></i>
+        <li id="profile">Profile</li>
+    </a>
+    <a href="#"><i class="fa-solid fa-gear"></i>
+        <li id="settings">Settings</li>
+    </a>
+    <button>
+        <a href="#">
+            <li id="logout">Logout</li>
+        </a>
+    </button>
+<?php else: ?>
+    <button id="login">
+        <a href="login.php">
+            <li id="login2">Login</li>
+        </a>
+    </button>
+    <a href="register.php">
         <li>Register</li>
-    <?php endif; ?>
-</ul>
+    </a>
+
+<?php endif; ?>
